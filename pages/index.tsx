89806371105819bb4e62b892165ed4d7df67066e
Keli Booster <kbooster17@gmail.com>
2022-04-28 20:27:44 +0000

    update footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,16 +50,7 @@ const Home: NextPage = () => {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        Built by <a href="https://www.linkedin.com/in/kelibst/">Kelibst</a>
       </footer>
     </div>
   );
